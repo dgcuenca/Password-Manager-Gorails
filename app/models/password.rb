6878c27b0b441bb33f8db93710ca6b09b56438db
diego <dgcuenca@gmail.com>
2023-11-22 12:31:57 -0500

    Encrypt fields

--- app/models/password.rb
+++ app/models/password.rb
@@ -2,5 +2,6 @@ class Password < ApplicationRecord
   has_many :user_passwords
   has_many :users, through: :user_passwords
 
-  
+  encrypts :username, deterministic:true #allows to query agains the username since a url can have multiple username, like netflix with different users
+  encrypts :password
 end
