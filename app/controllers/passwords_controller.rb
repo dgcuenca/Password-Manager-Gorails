ded9e6e3b1176e77d6ad1973dbaa89fbd1eda60b
diego <dgcuenca@gmail.com>
2023-11-23 12:37:52 -0500

    Enable user enter data for create a new password

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -2,5 +2,10 @@ class PasswordsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @passwords = current_user.passwords
+  end
+
+  def new
+    @password = Password.new
   end
 end
