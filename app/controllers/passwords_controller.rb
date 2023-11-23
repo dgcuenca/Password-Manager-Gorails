dd55c8287138593853c130365b53291c9258e8da
diego <dgcuenca@gmail.com>
2023-11-23 12:51:46 -0500

    Process data from form to create a new password

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -8,4 +8,21 @@ class PasswordsController < ApplicationController
   def new
     @password = Password.new
   end
+
+  def create
+    #We use that association and this tell rails when instanciate this new password in memory It will automatically
+    #have the join table automatically set up for us
+    @password = current_user.passwords.new(password_params)
+    if @password.save
+      redirect_to @password
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def password_params
+    params.require(:password).permit(:url, :username, :password)
+  end
 end
--- app/models/password.rb
+++ app/models/password.rb
@@ -4,4 +4,8 @@ class Password < ApplicationRecord
 
   encrypts :username, deterministic:true #allows to query agains the username since a url can have multiple username, like netflix with different users
   encrypts :password
+
+  validates :url, presence: true
+  validates :username, presence: true
+  validates :password, presence: true
 end
